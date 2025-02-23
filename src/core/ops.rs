24643edf5fad9c52f36e1337d9d698d479fbd864
yusufshihata <yusufshihata2006@gmail.com>
2025-02-23 15:12:40 +0200

    tensor scalar multiplication implemenation

--- src/core/ops.rs
+++ src/core/ops.rs
@@ -1,5 +1,5 @@
 use crate::core::tensor::Tensor;
-use std::ops::{Add, Mul};
+use std::ops::{Add, Mul, MulAssign};
 
 impl Add for Tensor {
     type Output = Tensor;
@@ -7,6 +7,15 @@ impl Add for Tensor {
     fn add(self, rhs: Self) -> Self::Output {
         assert!(self.shape == rhs.shape);
         let data = self.data + rhs.data;
+        Tensor::new(data, self.shape.clone(), self.requires_grad)
+    }
+}
+
+impl Mul<f64> for Tensor {
+    type Output = Tensor;
+
+    fn mul(self, scalar: f64) -> Self::Output {
+        let data = self.data * scalar;
         Tensor::new(data, self.shape.clone(), self.requires_grad)
     }
 }
--- test/ops_tests.rs
+++ test/ops_tests.rs
@@ -4,11 +4,20 @@ mod tests {
     use neura::core::tensor::Tensor;
 
     #[test]
-    fn add_tensors() {
+    fn add_tensors_test() {
         let t1 = Tensor::ones(&vec![2, 2], false);
         let t2 = Tensor::ones(&vec![2, 2], false);
         let t3 = t1 + t2;
 
         assert_eq!(t3.data[[0, 0]], 2.0);
+    }
+
+    #[test]
+    fn scalar_mul_test() {
+        let t1 = Tensor::ones(&vec![2,2], false);
+
+        let t2 = t1 * 2.0;
+
+        assert_eq!(t2.data[[0, 0]], 2.0);
     }
 }
